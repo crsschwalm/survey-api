a6510b348b6fc0b24b8b6202f88e13d2e8d74cce
Carson Schwalm <cschwalm@indiana.edu>
2018-07-04 13:34:22 -0400

    update readme and package.json

---
+++ README.md
@@ -0,0 +1,19 @@
+# DMI Survey
+### An app built with create-react-app
+
+## How to use
+Install it and run:
+
+```
+yarn
+cd client
+yarn
+yarn dev
+```
+
+## Idea behind the example
+DMI Survey application for understanding the basic concepts of JavaScript development.
+* Utilizes AWS - Heroku application Pipeline and MongoDB
+* Utilizes Node Express for server-side functionality
+* Utilizes React for front-end development
+* Authentication via Passport Admin account
--- server.js
+++ server.js
@@ -4,17 +4,15 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// API calls
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
 
 if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
+  app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
