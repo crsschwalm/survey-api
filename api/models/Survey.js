f104f3201eaf4a0baba20727bc3b962776d67c90
Carson Schwalm <crsschwalm@gmail.com>
2018-11-18 17:50:34 -0500

    Update/new reqs (#9)
    * validate and batch updload survey
    
    * yes no field schema
    
    * update default active status

--- api/models/Survey.js
+++ api/models/Survey.js
@@ -2,25 +2,46 @@ const mongoose = require('mongoose');
 const { SelectFromSchema } = require('./SelectFrom');
 const { CheckAllSchema } = require('./CheckAll');
 const { TextInputSchema } = require('./TextInput');
+const { YesNoSchema } = require('./YesNo');
 const { FieldSchema } = require('./Field');
 const { User } = require('./User');
 
 const Schema = mongoose.Schema;
 
 const SurveySchema = new Schema({
-    authorRef: {
-        type: Schema.Types.ObjectId,
-        ref: User
-    },
-    name: String,
-    description: String,
-    fields: [FieldSchema],
-    startDate: { type: Date, default: Date.now },
-    endDate: { type: Date }
+  authorRef: {
+    type: Schema.Types.ObjectId,
+    ref: User
+  },
+  name: String,
+  description: String,
+  fields: [FieldSchema],
+  startDate: { type: Date, default: Date.now },
+  endDate: { type: Date },
+  timeStamp: { type: Date, default: Date.now },
+  isActive: Boolean
 });
 
 SurveySchema.path('fields').discriminator('SelectFrom', SelectFromSchema);
 SurveySchema.path('fields').discriminator('CheckAll', CheckAllSchema);
 SurveySchema.path('fields').discriminator('TextInput', TextInputSchema);
+SurveySchema.path('fields').discriminator('YesNo', YesNoSchema);
+
+//default active status by end date in future
+SurveySchema.pre('save', async function(next) {
+  const survey = this;
+  try {
+    if (survey.isActive === undefined) {
+      const currentTime = new Date();
+      const startDate = new Date(survey.startDate);
+      const endDate = new Date(survey.endDate);
+      survey.isActive = endDate - startDate > 0 && endDate - currentTime > 0;
+    }
+    next();
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
 
 module.exports = mongoose.model('Survey', SurveySchema);
--- api/services/surveyService.js
+++ api/services/surveyService.js
@@ -3,69 +3,105 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
 module.exports = {
-    findSurveyById: (req, res) => {
-        const surveyId = req.params.id;
-        const showAnswers = !!req.query['show-answers'];
-        Survey.findById(surveyId)
-            .then(survey =>
-                showAnswers ?
-                    res.json(survey) :
-                    res.json(hideAnswers(survey))
-            )
-            .catch(err => res.status(401).send(err))
-    },
+  findSurveyById: (req, res) => {
+    const surveyId = req.params.id;
+    const showAnswers = req.query['show-answers'] === 'true';
+    Survey.findById(surveyId)
+      .then(
+        survey =>
+          showAnswers ? res.json(survey) : res.json(hideAnswers(survey))
+      )
+      .catch(err => res.status(401).send(err));
+  },
 
-    findSurveysByAuthorId: (req, res) => {
-        const authorId = req.params.id;
-        Survey.find({ authorRef: authorId })
-            .then(survey => res.json(survey))
-            .catch(err => res.status(401).send(err))
-    },
+  findSurveysByAuthorId: (req, res) => {
+    const authorId = req.params.id;
+    Survey.find({ authorRef: authorId })
+      .then(survey => res.json(survey))
+      .catch(err => res.status(401).send(err));
+  },
 
-    deleteSurveyById: (req, res) => {
-        const surveyId = req.params.id;
-        Survey.findByIdAndRemove(surveyId)
-            .then(() => res.json({ message: 'Survey successfully deleted!' }))
-            .catch(err => res.status(401).send(err))
-    },
+  deleteSurveyById: (req, res) => {
+    const surveyId = req.params.id;
+    Survey.findByIdAndRemove(surveyId)
+      .then(() => res.json({ message: 'Survey successfully deleted!' }))
+      .catch(err => res.status(401).send(err));
+  },
 
-    updateSurveyById: (req, res) => {
-        const surveyId = req.params.id;
-        Survey.findByIdAndUpdate(surveyId, { $set: req.body }, { new: true })
-            .then(survey => res.json(survey))
-            .catch(err => res.status(401).send(err))
-    },
+  updateSurveyById: (req, res) => {
+    const surveyId = req.params.id;
+    Survey.findByIdAndUpdate(surveyId, { $set: req.body }, { new: true })
+      .then(survey => res.json(survey))
+      .catch(err => res.status(401).send(err));
+  },
 
-    findAllSurveys: (req, res) => {
-        Survey.find()
-            .then(surveys => res.json(surveys))
-            .catch(err => res.status(401).send(err))
-    },
+  findAllSurveys: (req, res) => {
+    Survey.find()
+      .then(surveys => res.status(200).json(surveys))
+      .catch(err => res.status(401).send(err));
+  },
 
-    createSurvey: (req, res) => {
-        const {
-            authorRef,
-            name,
-            description,
-            fields,
-            startDate,
-            endDate
-        } = req.body;
-        const survey = new Survey();
-        survey.authorRef = authorRef;
-        survey.name = name;
-        survey.description = description;
-        survey.fields = fields;
-        survey.startDate = startDate;
-        survey.endDate = endDate;
-        survey.save()
-            .then(survey => res.json({ message: 'Survey successfully added!', id: survey._id }))
-            .catch(err => res.status(401).send(err))
-    },
-}
+  createSurvey: (req, res) => {
+    const saveSurveys = [].concat(req.body).map(surveyRequest =>
+      validateRequest(surveyRequest)
+        .then(saveSurvey)
+        .catch(handleBadSurveyReq)
+    );
 
-const hideAnswers = (surveyToTake) => {
-    const surveyWithEmptyAnswers = surveyToTake;
-    surveyWithEmptyAnswers.fields.forEach(field => field.expectedResponse = null)
-    return surveyWithEmptyAnswers;
-}
+    return Promise.all(saveSurveys)
+      .then(responses => {
+        //respond 'success' if any surveys are saved
+        if (responses.filter(response => response.success).length > 0) {
+          return res.status(200).json(responses);
+        }
+        throw responses;
+      })
+      .catch(error => res.status(401).send(error));
+  }
+};
+
+const hideAnswers = surveyToTake => {
+  const surveyWithEmptyAnswers = surveyToTake;
+  surveyWithEmptyAnswers.fields.forEach(
+    field => (field.expectedResponse = null)
+  );
+  return surveyWithEmptyAnswers;
+};
+
+const saveSurvey = surveyInfo => {
+  const survey = new Survey(surveyInfo);
+  return survey
+    .save()
+    .then(handleGoodSurveyReq)
+    .catch(error => {
+      throw error;
+    });
+};
+
+const validateRequest = request => {
+  const { authorRef, name, description, fields } = request;
+
+  return new Promise((resolve, reject) => {
+    isUndefined(authorRef) && reject('Required Fields Missing: "authorRef"');
+    isUndefined(name) && reject('Required Fields Missing: "name"');
+    isUndefined(description) &&
+      reject('Required Fields Missing: "description"');
+    isUndefined(fields) && reject('Required Fields Missing: "fields"');
+
+    resolve(request);
+  });
+};
+
+const isUndefined = element => element === undefined;
+
+const handleBadSurveyReq = error => ({
+  message: 'Survey failed to add',
+  success: false,
+  error: JSON.stringify(error)
+});
+
+const handleGoodSurveyReq = survey => ({
+  message: 'Survey successfully added!',
+  success: true,
+  id: survey._id
+});
