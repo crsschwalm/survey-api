a757b97d77a414fe036cc7cc6e60744f44631a94
crsschwalm <crsschwalm@gmail.com>
2018-12-03 09:47:40 -0500

    move endpoint list to /api

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,6 +4,9 @@ const responseRoutes = require('./response');
 const userRoutes = require('./user');
 const Router = express.Router();
 const authenticate = require('../services/authenticationService');
+const listEndpoints = require('express-list-endpoints');
+
+Router.get('/', (req, res) => res.json(listEndpoints(Router)));
 
 Router.use('/survey', surveyRoutes);
 Router.use('/response', authenticate, responseRoutes);
