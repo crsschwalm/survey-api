439841ce225b3a1d1b7d81d1a050036a4a31f539
crsschwalm <crsschwalm@gmail.com>
2019-01-15 19:38:19 -0500

    extract creds

--- api/services/authenticationService.js
+++ api/services/authenticationService.js
@@ -8,24 +8,17 @@ mongoose
 
 async function authenticate(req, res, next) {
   if (req.session && req.session.userId) {
+    console.log('req.session.userId :', req.session.userId);
     return next();
   }
 
-  const encryptedCredentials = parseAuthorizationHeader(
-    req.headers.authorization
-  );
-  console.log('encryptedCredentials :', encryptedCredentials);
-  const stringCredentials = base64.decode(encryptedCredentials);
-  console.log('stringCredentials :', stringCredentials);
-  const credentials = parseAuthorizationString(stringCredentials);
-  console.log('credentials :', credentials);
+  const credentials = parseCredentials(req);
 
   try {
     const { _id } = await User.authenticate(
       credentials.username,
       credentials.password
     );
-    console.log('_id :', _id);
     req.session.userId = _id;
     res.cookie('userId', _id, { maxAge: 900000, httpOnly: false });
     res.cookie('loggedIn', true, { maxAge: 900000, httpOnly: false });
@@ -37,6 +30,25 @@ async function authenticate(req, res, next) {
 }
 
 module.exports = authenticate;
+
+const parseCredentials = req => {
+  let basicAuthCredentials = {};
+  try {
+    const encryptedCredentials = parseAuthorizationHeader(
+      req.headers.authorization
+    );
+    const stringCredentials = base64.decode(encryptedCredentials);
+    basicAuthCredentials = parseAuthorizationString(stringCredentials);
+  } catch (err) {
+    console.error('error in basic auth: ', err);
+  }
+
+  validateUserCredentials(basicAuthCredentials) ||
+    validateUserCredentials(req.body);
+  return credentials;
+};
+
+const validateUserCredentials = user => !!user.username && !!user.password;
 
 const parseAuthorizationHeader = header => {
   const tokens = header.split(' ');
